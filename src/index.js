c4e39583b48d9039861c6aa25139b14919a902b0
Jonny Thaw <hi@jthaw.me>
2018-12-04 15:19:47 +0000

    Allowed app to now look up URL from text file on desktop

--- src/index.js
+++ src/index.js
@@ -3,33 +3,43 @@ const app = electron.app
 
 const BrowserWindow = electron.BrowserWindow
 
-const path = require( 'path' )
-const url = require( 'url' )
+const path = require( 'path' );
+const url = require( 'url' );
+const fs = require( 'fs' );
+const homedir = require('os').homedir();
 
-let mainWindow
+let mainWindow;
+let displayTxtFile = path.join(homedir, 'Desktop', 'display.txt');
+let urlTarget;
+
+if (fs.existsSync(displayTxtFile)) {
+    urlTarget = fs.readFileSync(displayTxtFile);
+}
 
 function createWindow () {
     mainWindow = new BrowserWindow( {
         width: 640,
         height: 480,
-        backgroundColor: '#fff071',
+        backgroundColor: '#000',
         kiosk: true
     } )
 
     mainWindow.loadURL( url.format( {
-        pathname: path.join( __dirname, 'index.html' ),
+        pathname: path.join( __dirname, `index.html` ),
         protocol: 'file:',
         slashes: true
     } ) );
 
     // mainWindow.webContents.openDevTools()
 
+    mainWindow.urlTarget = urlTarget;
+
     mainWindow.on( 'closed', function () {
         mainWindow = null
     } )
 }
 
-app.on( 'ready', createWindow )
+app.on( 'ready', createWindow)
 
 app.on( 'window-all-closed', function () {
     app.quit()
@@ -42,5 +52,5 @@ app.on( 'activate', function () {
 } )
 
 app.on( 'browser-window-blur', function() {
-    app.focus();
+    // app.focus();
 } )
--- src/renderer.js
+++ src/renderer.js
@@ -1,4 +1,11 @@
-var browser = document.getElementById( 'browser' )
-var url = require( 'url' )
-var url = 'http://gmdlcc.com'
-browser.src = url
+var electron = require('electron');
+
+var currentWindow = electron.remote.getCurrentWindow();
+var browser = document.getElementById('browser');
+var url = currentWindow.urlTarget;
+
+if (!url) {
+    document.body.innerHTML = "<div class='error'>No URL in ~/Desktop/display.txt or there is no ~/Desktop/display.txt</div>";
+} else {
+    browser.src = url.toString();
+}
